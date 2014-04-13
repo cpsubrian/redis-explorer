ee9f209b82cb5c7b5a2bcef874cee11406316e22
Brian Link <cpsubrian@gmail.com>
2014-04-13 14:14:46 -0700

    Adds copy key to clipboard button.

--- app/js/views/records/records_table_row_view.js
+++ app/js/views/records/records_table_row_view.js
@@ -3,14 +3,29 @@ define(function (require) {
   var app = require('app')
     , Marionette = require('marionette')
     , bootstrap = require('bootstrap')
-    , RecordDetailView = require('views/records/record_detail_view');
+    , RecordDetailView = require('views/records/record_detail_view')
+    , clipboard = gui.Clipboard.get();
 
   var RecordsTableRowView = Marionette.ItemView.extend({
     tagName: 'tr',
     template: require('hbs!records/records_table_row'),
 
+    ui: {
+      type: '.type i',
+      operations: '.operations i',
+      copy: '.operations .copy'
+    },
+
     events: {
-      'click': 'onClick'
+      'click': 'onClick',
+      'click .operations .copy': 'onClickCopy',
+      'mouseleave .operations .copy': 'onMouseleaveCopy',
+    },
+
+    onDomRefresh: function () {
+      this.copyTitle = this.ui.copy.attr('title');
+      this.ui.type.tooltip();
+      this.ui.operations.tooltip();
     },
 
     templateHelpers: function () {
@@ -56,6 +71,17 @@ define(function (require) {
       }));
       this.$el.addClass('selected');
       this.$el.siblings('tr').removeClass('selected');
+    },
+
+    onClickCopy: function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      clipboard.set(this.model.id, 'text');
+      this.ui.copy.attr('title', 'Copied!').tooltip('fixTitle').tooltip('show');
+    },
+
+    onMouseleaveCopy: function (e) {
+      this.ui.copy.attr('title', this.copyTitle).tooltip('fixTitle');
     }
   });
 
